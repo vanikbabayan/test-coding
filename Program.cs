310257a56637c27bc61422eb53646a8597cba89a
Vanik Babayan <vanikobabayan@gmail.com>
2021-01-07 19:16:05 +0400

    changed the message to barev

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace test_coding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Barev!");
         }
     }
 }
